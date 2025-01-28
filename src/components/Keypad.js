749dccde88d64ce868e21625c1825021acd682a4
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-01-28 03:39:39 -0500

    Passes all tests

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,10 @@
+import React from 'react'
+
 // Code Keypad Component Here
 
 function Keypad (){
     return (
-        <div></div>
+        <input type='password' onChange={() => console.log("Entering password...")}></input>
     )
 }
 
